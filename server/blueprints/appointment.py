7ee02b13506732d1598860f31e70de3439990e36
Mahboob Attar <134129628+Mahboob-Attar@users.noreply.github.com>
2025-09-12 19:03:02 +0530

    update appointment blueprint

--- server/blueprints/appointment.py
+++ server/blueprints/appointment.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-# Create a separate Blueprint for appointment portal
+
 appointment_bp = Blueprint('appointment', __name__,url_prefix="/appointment")
 
 @appointment_bp.route('/appointment')
